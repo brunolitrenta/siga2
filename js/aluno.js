22e6d45a317b4ab86fa9116fb9d3b8a394345147
Bruno Litrenta <144285558+brunolitrenta@users.noreply.github.com>
2023-10-11 19:58:20 -0300

    Create aluno.js

---
+++ js/aluno.js
@@ -0,0 +1,17 @@
+//var declarations
+  const pnomeElement = document.getElementbyId('pnome')
+
+//function declarations
+  function getUserData(){
+    const usuario = JSON.parse(window.sessionStorage.getItem("usuario"))
+    if(!usuario){
+      alert("Dados não encontrados.")
+      settimeout(()=>{window.location.href="login.html"},5000)
+      return;
+    }
+  pnome.innerText = usuario.nome;
+    
+  }
+
+//actions
+  getUserData();
